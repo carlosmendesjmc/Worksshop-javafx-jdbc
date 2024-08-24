d7484d6f76ceb19562226dab6200970e89f321de
carlosmendesjmc <carlosmendesjmc@gmail.com>
2024-08-24 19:14:57 -0300

    Main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,33 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			/*
+			 * Criando uma instacia do metodo Loader, para manipular a tela antes de
+			 * carregar
+			 */
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load(); /* chama o loader e carrega a view */
+			Scene mainScene = new Scene(parent); /* cria o objeto pra primeira cena principal */
+			primaryStage.setScene(mainScene);/* palco da sena esta sendo setada como principal */
+			primaryStage.setTitle("Sample JavaFX application"); /* definindo um titulo da cena principal */
+			primaryStage.show(); /* aqui mostra o palco */
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
